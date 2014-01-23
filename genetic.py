90ea88ca2329abb7576f6f973efdc275328649bc
Tural Gurbanov <madcat1991@gmail.com>
2014-01-23 23:05:46 +0400

    Simple genetic algorithm

---
+++ genetic.py
@@ -0,0 +1,90 @@
+#coding: utf-8
+
+import math
+import random
+ 
+STEPS = 100
+ALPHA = 0.85
+TEMP_0 = 100 
+M = 600
+
+
+def genetic_algorithm(number, capacity, weight_cost):
+    start_sol = init_solution(weight_cost, max_weight=capacity)
+    best_cost, solution = simulate(start_sol, weight_cost, max_weight=capacity)
+    best_combination = [0] * number
+    for idx in solution:
+        best_combination[idx] = 1
+    return best_cost, best_combination
+
+
+def init_solution(weight_cost, max_weight):
+    """Used for initial solution generation.
+    By adding a random item while weight is less max_weight
+    """
+    solution = []
+    allowed_positions = range(len(weight_cost))
+    while len(allowed_positions) > 0:
+        idx = random.randint(0, len(allowed_positions) - 1)
+        selected_position = allowed_positions.pop(idx)
+        if get_cost_and_weight_of_knapsack(solution + [selected_position], weight_cost)[1] <= max_weight:
+            solution.append(selected_position)
+        else:
+            break
+    return solution
+
+
+def get_cost_and_weight_of_knapsack(solution, weight_cost):
+    """Get cost and weight of knapsack - fitness function
+    """
+    cost, weight = 0, 0
+    for item in solution:
+        weight += weight_cost[item][0]
+        cost += weight_cost[item][1]
+    return cost, weight
+
+
+def moveto(solution, weight_cost, max_weight):
+    """All possible moves are generated"""
+    moves = []
+    for idx, _ in enumerate(weight_cost):
+        if idx not in solution:
+            move = solution[:]
+            move.append(idx)
+            if get_cost_and_weight_of_knapsack(move, weight_cost)[1] <= max_weight:
+                moves.append(move)
+    for idx, _ in enumerate(solution):
+        move = solution[:]
+        del move[idx]
+        if move not in moves:
+            moves.append(move)
+    return moves
+
+
+def simulate(solution, weight_cost, max_weight):
+    """Simulated annealing approach for Knapsack problem"""
+    temperature = TEMP_0
+
+    best = solution
+    best_cost = get_cost_and_weight_of_knapsack(solution, weight_cost)[0]
+
+    current_sol = solution
+    while True:
+        current_cost = get_cost_and_weight_of_knapsack(best, weight_cost)[0]
+        for i in range(0, STEPS):
+            moves = moveto(current_sol, weight_cost, max_weight)
+            idx = random.randint(0, len(moves) - 1)
+            random_move = moves[idx]
+            delta = get_cost_and_weight_of_knapsack(random_move, weight_cost)[0] - best_cost
+            if delta > 0:
+                best = random_move
+                best_cost = get_cost_and_weight_of_knapsack(best, weight_cost)[0]
+                current_sol = random_move
+            else:
+                if math.exp(delta / float(temperature)) > random.random():
+                    current_sol = random_move
+
+        temperature *= ALPHA
+        if current_cost >= best_cost or temperature <= 0:
+            break
+    return best_cost, best
